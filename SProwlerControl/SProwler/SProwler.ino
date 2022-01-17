99ffad3739600c14af51541caabbaa846207fc6d
Edward Corlett <eddycorlett@gmail.com>
2022-01-17 18:13:25 -0500

    Added xMovement function

--- SProwlerControl/SProwler/SProwler.ino
+++ SProwlerControl/SProwler/SProwler.ino
@@ -1,30 +1,59 @@
+/*
 int motor1pin1 = 2;
 int motor1pin2 = 3;
 
 int motor2pin1 = 4;
 int motor2pin2 = 5;
+ */
+
+  int d2 = 2;
+  int d3 = 3;
+  int d4 = 4;
+  int d5 = 5;
 
 void setup() {
   // put your setup code here, to run once:
-  pinMode(motor1pin1, OUTPUT);
-  pinMode(motor1pin2, OUTPUT);
-  pinMode(motor2pin1, OUTPUT);
-  pinMode(motor2pin2, OUTPUT);
+  //pinMode(port#, output or input)
+
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:   
+  /*
   digitalWrite(motor1pin1, HIGH);
   digitalWrite(motor1pin2, LOW);
+   */
+  // put your main code here, to run repeatedly:
 
-  digitalWrite(motor2pin1, HIGH);
-  digitalWrite(motor2pin2, LOW);
-  delay(1000);
+  //check x sensor and assign parameter value
 
-  digitalWrite(motor1pin1, LOW);
-  digitalWrite(motor1pin2, HIGH);
+  /*
+    
+   xMovementControl(directionparameter);
+  
+   */
+  
+}
 
-  digitalWrite(motor2pin1, LOW);
-  digitalWrite(motor2pin2, HIGH);
-  delay(1000);
+void xMovementControl(directionparameter){
+
+/*
+  switch(directionparameter):
+
+  directionparameter = LEFT;
+
+    //step left code
+ 
+  directionparameter = RIGHT;
+
+    //step right code
+  
+  directionparameter = STOP;
+  
+    //do nothing
+    
+    }
+ 
+ */
+  
+
 }
