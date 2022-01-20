5646687802d38eab0450d0b6489a97c8aaf6cde4
kk <pepperk211@gmail.com>
2022-01-20 10:20:31 -0500

    added practice function

--- SProwlerControl/SProwler/SProwler.ino
+++ SProwlerControl/SProwler/SProwler.ino
@@ -1,23 +1,15 @@
-/*
-int motor1pin1 = 2;
-int motor1pin2 = 3;
-
-int motor2pin1 = 4;
-int motor2pin2 = 5;
- */
-
-  int d2 = 2;
-  int d3 = 3;
-  int d4 = 4;
-  int d5 = 5;
-
+// decloration of variables
+int directionParameter = 0;
+int func1;
 void setup() {
   // put your setup code here, to run once:
   //pinMode(port#, output or input)
 
 }
 
+//main loop
 void loop() {
+  func1 =  xMovementControl(directionParameter);
   /*
   digitalWrite(motor1pin1, HIGH);
   digitalWrite(motor1pin2, LOW);
@@ -34,7 +26,8 @@ void loop() {
   
 }
 
-void xMovementControl(directionparameter){
+// decloration of functions
+int xMovementControl(int directionParameter){
 
   /*
    code 
