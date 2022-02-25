3340bc2ea1559372a1a0f8ed58c7932a78cd7f5f
Justin Green <jag19t@my.fsu.edu>
2022-02-25 09:18:29 -0500

    Started State machine
    S

--- practice stepper motor/Justin_Version/Justin_Version.ino
+++ practice stepper motor/Justin_Version/Justin_Version.ino
@@ -4,17 +4,26 @@
 #include <LIDARLite_v4LED.h>
 
  
- //Global variables
+ //Global variables/////
+ //motorX
 const int buttonPin = 12;
 const int stepPin = 6;
 const int dirPin = 7;
 const int enPin = 8;
 const int delayValue = 200;
 const int stepsPerRev = 1600; //200000 or a number that doesnt give a warning
+//Garmin
 const int yDirTime = 2000;    //time it takes for rod to scope out and back in
 LIDARLite lidarLite;          //Garmin Variable
 int cal_cnt = 0;              //Garmin Variable
 int dist;                     //Garmin Distance Variable
+
+//Brush motors
+int motor1Pin1 = 2;
+int motor1Pin2 = 3;
+int motor2Pin1 = 4;
+int motor2Pin2 = 5;
+int buttonPin = 22;
 
 // function declorations
 int xDirC;
@@ -22,7 +31,7 @@ int xDirCC;
 int buttonFunc;
 int motorStop;
 int revs = 0;
-int buttonState = 0; //represents if sensor goes LOW
+int stopMotorX()
 
 void setup() {
   // setting pins as inputs or outputs
@@ -49,6 +58,25 @@ void setup() {
   }
 }
 */
+int Brushoff(){
+  digitalWrite(motor1Pin1, LOW);//left
+  digitalWrite(motor2Pin2, LOW);//right
+}
+
+int Brushon(){
+  digitalWrite(motor1Pin1, HIGH); //left CW
+  digitalWrite(motor2Pin2, HIGH);//right CC
+}
+
+/*int Brushturn(){
+  delay(1000);
+      digitalWrite(motor1Pin1, LOW); //left CC
+      digitalWrite(motor2Pin2, LOW);//right CC
+  delay(1000); //once we know x distance is met 
+      digitalWrite(motor1Pin1, HIGH); //left CW
+      digitalWrite(motor2Pin2, HIGH);//right CC
+}*/
+
 int xDirStepCounterClockwise(){
   digitalWrite(dirPin, LOW);
   // makes 200 pulses which equals 1 revolution
@@ -59,18 +87,19 @@ int xDirStepCounterClockwise(){
     delayMicroseconds(delayValue);
     }
 }
-int stopMotor(){//stops motor for a certain amount of time or until button is pressed
+int stopMotorX(){//stops motor for a certain amount of time or until button is pressed
   for(int x = 0; x < stepsPerRev; x++){
     digitalWrite(enPin, HIGH);
     delayMicroseconds(delayValue);
   }
 }
-void move_motors(int dist){
+void move_motorX(int dist)
+{
   if (dist >= 50 ){
         xDirStepCounterClockwise();
       }
   else {
-      stopMotor();
+      stopMotorX();
       }
   }
 void loop() {
@@ -87,7 +116,7 @@ void loop() {
   cal_cnt = cal_cnt % 100;
   
   //Call To move motors
-  move_motors(dist);
+  move_motorX(dist);
 
   // Display distance
   Serial.print(dist);
@@ -96,24 +125,68 @@ void loop() {
   delay(10);
 }
 
-/*
- * int pressButton(){
-  buttonState = digitalRead(buttonPin);
+state_machine(uint8_tsensors)
+{
+  //Start up Delay
+  case zero:
+  {
+    delayMicroseconds(1000);//delay for system
+    //GarminInitial Measurement
+    //use sensor to record total distance and name variable dist1 for calculations
+  if ( cal_cnt == 0 ) {
+    dist1 = lidarLite.distance();      // With bias correction
+  } else {
+    dist1 = lidarLite.distance(false); // Without bias correction
+  }
+  
+  // Increment reading counter
+  cal_cnt++;
+  cal_cnt = cal_cnt % 100;
+  
+  // Display distance
+  Serial.print(dist1);
+  Serial.println(" cm");
+   
+  }
+  break;
+  
+  //Initialize all components
+  case one:
+  {
+    //initialize all motors and sensors
+    //start brush motors
+  }
+  break;
+  
+  //Robot Down in Y-direction
+  case two:
+  {
+    //Turn on y direction motor
+  }
+  break;
 
-  // HIGH = pressed
-  if (buttonState == HIGH){
-    //turn off stepper motor
-    digitalWrite(enPin, HIGH);
-    Serial.print("Button has been pressed");
-    Serial.print(buttonState);
+  //Robot Up in Y-direction
+  case three:
+  {
+    //stop y-direction motor and delay
+    //Reverse y-direction motor
   }
-  else {
-    //continue motor oporation as normal 
-    digitalWrite(enPin, LOW);
-    Serial.print("Button NOT pressed");
-    Serial.print(buttonState);
+  break;
+
+  //Move robot in X-direction
+  case four:
+  {
+   //Turn on x direction motor for either set amount or until it has traveled the brush width
+   xDirStepCounterClockwise();//motorx on 
   }
-  //resetting button state
-  buttonState = 0;
+  break;
+
+  //Stop the robot 
+  case stop:
+  {
+    stopMotorX();
+    //Create function to stopmotorY
+  }
+  break;
+  
 }
- */
